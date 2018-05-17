82b9c894af631fa67d8e729dead0479598e42bf9
education-PC\education <mas-ono@seraku.co.jp>
2018-05-17 12:53:16 +0900

    201805171253

--- JS+jQuery/js/task.js
+++ JS+jQuery/js/task.js
@@ -44,13 +44,17 @@ $(function(){
           e.preventDefault();
     });
 });
+/*課題6*/
+
+
 /*課題7*/
 $(function() {
 	$('.tab li').click(function() {
 		var index = $('.tab li').index(this);
-		$('.content li').css('display','none');
-		$('.content li').eq(index).css('display','block');
-		$('.tab li').removeClass('select');
-		$(this).addClass('select')
+		$('.tab_content li').css('display','none');
+		$('.tab_content li').eq(index).css('display','block');
+		$('.tab li').removeClass('tab_select');
+    $('.tab_content li').removeClass('hide');
+		$(this).addClass('tab_select');
 	});
 });
